009bef1737e087b3037da90db7bee69313ff1053
Hunde-D <hunddh2025@gmail.com>
2024-07-03 23:56:08 +0300

    db conn helper

---
+++ src/notes.js
@@ -0,0 +1,40 @@
+import { insert, getDB, saveDB } from "./db.js";
+
+export const newNote = async (note, tags) => {
+  try {
+    const data = {
+      tags,
+      content: note,
+      id: Date.now(),
+    };
+    await insert(data);
+    return data;
+  } catch (err) {
+    console.error("new note error:", err);
+  }
+};
+export const getAllNotes = async () => {
+  const db = await getDB();
+  return db.notes;
+};
+
+export const findNotes = async (filter) => {
+  const notes = await getAllNotes();
+  return notes.filter((note) =>
+    note.content.toLowerCase().includes(filter.toLowerCase())
+  );
+};
+export const removeNote = async (id) => {
+  const notes = await getAllNotes();
+  const match = notes.find((note) => note.id === id);
+
+  if (match) {
+    const newNotes = notes.filter((note) => note.id !== id);
+    await saveDB({ notes: newNotes });
+    return `\nNote removed successfully \n id: ${id}`;
+  }
+  return `\nNote not found \n id:${id}`;
+};
+export const removeAllNotes = async () => {
+  await saveDB({ notes: [] });
+};
